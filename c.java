4a2bb8a701f0db6944af4c51c6dc9348bfd0767c
vikas mantipalli <vikasmantipalli@gmail.com>
2026-02-09 10:22:03 +0530

    vvv

--- c.java
+++ c.java
@@ -2,8 +2,6 @@ import  java.util.*;
 public class c{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("cccchello");
-        System.err.println("hihello");
-
+        System.err.println("cccccccc");
     }
 }
