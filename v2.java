cfbaa18da868b321529a0a09df7f605270e9dfd8
vikas mantipalli <vikasmantipalli@gmail.com>
2026-02-09 21:27:08 +0530

    by v2

--- v2.java
+++ v2.java
@@ -2,6 +2,6 @@ import  java.util.*;
 public class v2{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("hi vikas");
+        System.out.println("by v2");
     }
 }
