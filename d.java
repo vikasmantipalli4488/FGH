55abcf1b088a6432e0d04cfced7c80f2eb50b1bd
vikas mantipalli <vikasmantipalli@gmail.com>
2026-02-09 10:02:57 +0530

    changes on d

--- d.java
+++ d.java
@@ -2,6 +2,6 @@ import  java.util.*;
 public class d{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("hello");
+        System.out.println("hihello");
     }
 }
